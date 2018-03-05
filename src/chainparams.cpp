ecf846853fa7de55a4004248c37ded5739a8dc28
sharjeel <manshasharjeel@gmail.com>
2018-03-06 02:47:24 +0500

    removing genisis and markle addresses

--- src/chainparams.cpp
+++ src/chainparams.cpp
@@ -61,7 +61,7 @@ public:
 
         hashGenesisBlock = genesis.GetHash();
 
-        assert(hashGenesisBlock == uint256("0x00000f639db5734b2b861ef8dbccc33aebd7de44d13de000a12d093bcc866c64"));
+        assert(hashGenesisBlock == uint256("0x"));
         assert(genesis.hashMerkleRoot == uint256("0xfa6ef9872494fa9662cf0fecf8c0135a6932e76d7a8764e1155207f3205c7c88"));
 
         vSeeds.push_back(CDNSSeedData("seed1.innoket.org", "seed1.innoket.org"));
@@ -141,7 +141,7 @@ public:
         genesis.nNonce = 423087994;
 
         hashGenesisBlock = genesis.GetHash();
-        assert(hashGenesisBlock == uint256("0x0000082f5939c2154dbcba35f784530d12e9d72472fcfaf29674ea312cdf4c83"));
+        assert(hashGenesisBlock == uint256("0x"));
 
         vFixedSeeds.clear();
         vSeeds.clear();
