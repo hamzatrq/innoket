7c677f57dd97616d1eb71c272d6d77ce583eeca8
sharjeel <manshasharjeel@gmail.com>
2018-03-06 03:13:45 +0500

    Correting a small mistake

--- src/chainparams.cpp
+++ src/chainparams.cpp
@@ -61,13 +61,8 @@ public:
 
         hashGenesisBlock = genesis.GetHash();
 
-<<<<<<< HEAD
         assert(hashGenesisBlock == uint256("0x00000f639db5734b2b861ef8dbccc33aebd7de44d13de000a12d093bcc866c64"));
         assert(genesis.hashMerkleRoot == uint256("0x"));
-=======
-        assert(hashGenesisBlock == uint256("0x"));
-        assert(genesis.hashMerkleRoot == uint256("0xfa6ef9872494fa9662cf0fecf8c0135a6932e76d7a8764e1155207f3205c7c88"));
->>>>>>> ecf846853fa7de55a4004248c37ded5739a8dc28
 
         vSeeds.push_back(CDNSSeedData("seed1.innoket.org", "seed1.innoket.org"));
         vSeeds.push_back(CDNSSeedData("seed2.innoket.org", "seed2.innoket.org"));
